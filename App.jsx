acbf53862ae5aec92788ef165da520921a56acee
ApiiwDev <muh.raffi.ar@gmail.com>
2023-08-20 14:53:15 +0700

    'add'

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import {View, ToucableOpacity, StatusBar, StyleSheet} from 'react-native';
+const App = () => {
+    return(
+        <View></View>
+    );
+}
+const style = StyleSheet.create({});
+export default App;
